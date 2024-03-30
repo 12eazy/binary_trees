a9d11574ce8587faaf9a9d80e7917d29bd6c4391
Oyetola israel <oyetolaisreal01@gmail.com>
2024-03-29 19:22:56 -0700

    editing

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,18 +1,10 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_uncle - function that finds if a node is sibling
- * @node: node to check
- * Return: The uncle node
+ * binary_tree_depth - depth of specified node from root
+ * @tree: node to check the depth
+ * Return: 0 is it is the root or number of depth
  */
-
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-	{
-		return (NULL);
-	}
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
